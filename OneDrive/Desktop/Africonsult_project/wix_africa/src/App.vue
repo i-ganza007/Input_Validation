717cff96d3ca83d1e27dbb5f520a7439ad91fa6a
i-ganza007 <i.ganza@alustudent.com>
2025-04-05 21:59:00 +0200

    Restructuring the app

---
+++ OneDrive/Desktop/Africonsult_project/wix_africa/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <NavBar/>
+  <Hero/>
+  <ServiceSect/>
+</div>
+  
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Hero from './components/HeroComp.vue'
+import ServiceSect from './components/ServiceSect.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Hero,
+    ServiceSect
+  }
+}
+</script>
+
